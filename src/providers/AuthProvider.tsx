fbf8f39b765aab2b579e0f8f78f043f4af756fec
Yuki <khongbachofficial@gmail.com>
2026-06-04 03:17:46 +0700

    feat(auth): implement AuthContext and AuthProvider for authentication management

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import type { ReactNode } from "react";
+import type { Role } from "../constants/roles";
+import { AuthContext } from "../contexts/AuthContext";
+import type { AuthContextType } from "../types/auth";
+import { jwtDecode } from "jwt-decode";
+import api from "../services/api";
+
+interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  role: Role;
+}
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const accessToken = sessionStorage.getItem("accessToken");
+
+      if (!accessToken) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const decoded = jwtDecode<CustomJwtPayload>(accessToken);
+        const isExpired = decoded.exp ? decoded.exp * 1000 < Date.now() : false;
+
+        console.log("isExpried", decoded.exp);
+
+        if (isExpired) {
+          const response = await api.post("/auth/refresh", {});
+          console.log("data day", response.data);
+          const newAccessToken = response.data.data.accessToken;
+          const data = jwtDecode(newAccessToken);
+          console.log(data);
+          sessionStorage.setItem("accessToken", newAccessToken);
+
+          const newData = jwtDecode<CustomJwtPayload>(newAccessToken);
+          setUser({
+            id: newData.id,
+            fullName: newData.fullName,
+            email: newData.email,
+            role: newData.roles.at(0) ?? "MANGAKA",
+          });
+        } else {
+          setUser({
+            id: decoded.id,
+            fullName: decoded.fullName,
+            email: decoded.email,
+            role: decoded.roles.at(0) ?? "MANGAKA",
+          });
+        }
+      } catch (error) {
+        console.error("Invalid token:", error);
+        setUser(null);
+        sessionStorage.removeItem("accessToken");
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+  const login = async (loginPayload: loginPayload) => {
+    try {
+      const response = await api.post("/auth/login", loginPayload);
+      const accessToken = response.data.data.accessToken;
+
+      sessionStorage.setItem("accessToken", accessToken);
+
+      const decoded = jwtDecode<CustomJwtPayload>(accessToken);
+      setUser({
+        id: decoded.id,
+        fullName: decoded.fullName,
+        email: decoded.email,
+        role: decoded.roles.at(0) ?? "MANGAKA",
+      });
+    } catch (error) {
+      console.error("Login function error in Provider:", error);
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    setUser(null);
+  };
+
+  const value: AuthContextType = {
+    user,
+    isAuthenticated: !!user,
+    login,
+    logout,
+    loading,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
---
+++ src/types/auth.ts
@@ -0,0 +1,12 @@
+export interface AuthContextType {
+  user: {
+    id: number;
+    fullName: string;
+    email: string;
+    role: "MANGAKA" | "TANTOU" | "ADMIN" | "ASSISTANT";
+  } | null;
+  isAuthenticated: boolean;
+  login: (loginPayload: loginPayload) => Promise<void>;
+  logout: () => void;
+  loading: boolean;
+}
