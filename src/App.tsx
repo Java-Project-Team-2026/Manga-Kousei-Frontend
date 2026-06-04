39ad8298af4a24824db0511958dc7e96268bc80a
Yuki <khongbachofficial@gmail.com>
2026-06-04 12:50:21 +0700

    feat(routes): implement role-based dashboard redirection and add roleDashboardMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/admin/Dashboard";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { PublicRoute } from "./components/PublicRoute";
-import { MainLayout } from "./components/MainLayout";
+import { ProtectedRoute } from "./components/Routes/ProtectedRoute";
+import { PublicRoute } from "./components/Routes/PublicRoute";
+import { MainLayout } from "./components/layouts/MainLayout";
 import AdminRating from "./pages/admin/Rating";
+import MangakaDashboard from "./pages/mangaka/Dashboard";
+import { DashboardRedirect } from "./pages/DashboardRedirect";
 
 function App() {
   return (
@@ -13,10 +15,22 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Route>
+
+      <Route path="/dashboard" element={<DashboardRedirect />} />
+
       <Route element={<MainLayout />}>
         <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
-          <Route path="/dashboard" element={<AdminDashboard />} />
-          <Route path="/rating" element={<AdminRating />} />
+          <Route path="/admin">
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="rating" element={<AdminRating />} />
+          </Route>
+        </Route>
+        <Route element={<ProtectedRoute allowedRoles={["MANGAKA"]} />}>
+          <Route path="/mangaka">
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<MangakaDashboard />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
